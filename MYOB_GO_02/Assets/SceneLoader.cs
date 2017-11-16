463e24af605296ac4da717828404ee17a4ba7787
ChesterMYOB <chester.booker@myob.com>
2017-11-16 15:38:04 +1300

    first push

---
+++ MYOB_GO_02/Assets/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button yourButton;
+    public string ScenePath;
+
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(ScenePath);
+    }
+
+
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        LoadScene();
+    }
+}
